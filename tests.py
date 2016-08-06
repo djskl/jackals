f93d9f850fc6bb54c49852ca38d1a57b60d92629
djskl <448189234@qq.com>
2016-08-06 17:00:56 +0800

    reconstruct

--- tests.py
+++ tests.py
@@ -6,12 +6,13 @@ Created on Jul 31, 2016
 import unittest
 from websocket import create_connection
 import redis
+from utils import make_channel
 
 class Test(unittest.TestCase):
 
     def setUp(self):
-        self._channel = "foobar"
-        self.ws_client = create_connection("ws://127.0.0.1/%s"%self._channel)
+        self.path_info = "foobar"
+        self.ws_client = create_connection("ws://127.0.0.1/%s"%self.path_info)
         self.redis_conn = redis.StrictRedis()
         
     def tearDown(self):
@@ -23,10 +24,15 @@ class Test(unittest.TestCase):
         self.assertEqual(length, len(msg)+6)
         
     def test_redis_channel(self):
+        self._channel = make_channel(self.path_info)
         self.redis_conn.publish(self._channel, "hello")
-        msg = self.ws_client.recv()
+                
+        msg = self.ws_client.recv() # TODO: timeout
+        
         self.assertEquals(msg, "hello")
         
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
+    
+    
--- websocket_server/wsgi_ws_server.py
+++ websocket_server/wsgi_ws_server.py
@@ -1,47 +1,46 @@
 import uwsgi
-from sub.rds import Subscriber
-from sub.rds import make_redis_channels as make_channel
+from channels import RedisChannel
+from utils import make_channel
 
 def handle_client_msg(msg):
     if msg:
         print "receive message:", msg
     else:
         print "recevied an empty message"
-        
-        
+     
 class WSGIWebsocketServer(object):
     
     def __call__(self, env, sr):
         
-        channel = make_channel(env["PATH_INFO"])
+        channel_name = make_channel(env["PATH_INFO"])
         
-        if not channel:
+        if not channel_name:
             raise Exception("path_info is invalid")
         
-        sub = Subscriber()
-        sub.subscribe(channel)
+        outer_channel = RedisChannel()
+        outer_channel.subscribe(channel_name)
         
-        ws = self.upgrade_websocket(env, sr)
+        ws_conn = self.upgrade_websocket(env, sr)
         
-        while ws and not ws.closed:
+        while ws_conn and not ws_conn.closed:
             
-            ws_fd = ws.get_file_descriptor()
-            rd_fd = sub.get_file_descriptor()
+            ws_fd = ws_conn.get_file_descriptor()
+            rd_fd = outer_channel.get_file_descriptor()
             
             ready = self.select([ws_fd, rd_fd], [], [], 4)
             
             if not ready[0]:
-                ws.flush()
+                ws_conn.flush()
             else:
                 for fd in ready[0]:
                     if fd == ws_fd:
-                        msg = ws.receive()
+                        msg = ws_conn.receive()
                         handle_client_msg(msg)
                         
                     elif fd == rd_fd:
-                        msg = sub.get_message()
+                        msg = outer_channel.get_message(channel_name)
                         if msg:
-                            ws.send(msg)
+                            ws_conn.send(msg)
         
         return ""
     
